e411206a5ec657ba1b02813639e0cab21d7018bf
Vieira92 <vieiraonwork@gmail.com>
2020-08-11 17:13:43 +0100

    Project created

---
+++ src/main/java/com/demostore/projectwebservice/DemoStoreProjectWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demostore.projectwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStoreProjectWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStoreProjectWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demostore/projectwebservice/DemoStoreProjectWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demostore.projectwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStoreProjectWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
